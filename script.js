351c9a1adbbe171daccdefb4c9b46b3890d53a98
saff-it <saff_it@ymail.com>
2022-06-14 16:28:49 +0200

    exercise completed

--- script.js
+++ script.js
@@ -3,6 +3,8 @@
 let userDistance = parseInt( prompt ('Quanti Kilometri intendi percorrere?') );
 const userAge = parseInt( prompt ('Quanti anni hai?') );
 const ticketPrice = userDistance * 0.21;
+const ticketUnderAge = ticketPrice - ( (ticketPrice * 20) / 100);
+const ticketElderly = ticketPrice - ( (ticketPrice * 40) / 100);
 console.log(userDistance, userAge, ticketPrice);
 
 // Document
@@ -10,3 +12,20 @@ document.getElementById('user-distance').innerHTML = userDistance;
 document.getElementById('user-age').innerHTML = userAge;
 document.getElementById('ticket-price').innerHTML = ticketPrice;
 
+// Conditions
+
+if ( isNaN(userDistance) == true ) {
+    console.log('Qualcosa è andato storto; ricarica la pagina ed inserisci il numero di KM che intendi percorrere');
+
+} else if ( isNaN(userAge) == true ) {
+    console.log('Qualcosa è andato storto; ricarica la pagina ed inserisci il numero dei tuoi anni');
+
+} else {
+
+    if (userAge < 18) {
+        document.getElementById('ticket-price').innerHTML = ticketUnderAge;
+        
+    } else if (userAge > 65) {
+        document.getElementById('ticket-price').innerHTML = ticketElderly;
+    }
+}    
